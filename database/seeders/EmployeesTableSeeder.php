ec62d38b5d482eb3a1e09038bd181d437f50f989
moneruzzamanju <moneruzzamanju@gmail.com>
2022-09-29 12:50:18 +0600

    Create EmployeesTableSeeder for Seed Data

---
+++ database/seeders/EmployeesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmployeesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('employees')->insert([
+            'name' => 'moneruzzaman',
+            'basic' => 90000,
+        ]);
+    }
+}
